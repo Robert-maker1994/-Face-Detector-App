247729fe845968405e64ab75c96b3effdd77d767
Robert-maker1994 <robertpaullawson@gmail.com>
2020-09-08 12:34:26 +0100

    changing the local host

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ updateUser = (data) => {
   
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageUrl', {
+    fetch('https://nameless-reaches-51523.herokuapp.com//imageUrl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -96,7 +96,7 @@ updateUser = (data) => {
     .then(response => response.json())
         .then(response => {
           if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://nameless-reaches-51523.herokuapp.com//image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
